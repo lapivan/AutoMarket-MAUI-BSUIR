c0ebf859fcb4655d37939d108e70f849500caf4b
John Lapunou <lapunoujohn@gmail.com>
2026-03-22 22:38:25 +0300

    Persistense: Added EfRepository

---
+++ AutoMarket.Persistense/Repository/EfRepository.cs
@@ -0,0 +1,81 @@
+﻿using AutoMarket.Persistense.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using static System.Net.WebRequestMethods;
+
+namespace AutoMarket.Persistense.Repository
+{
+    public class EfRepository<T> : IRepository<T> where T : Entity
+    {
+        protected readonly AppDbContext _context;
+        protected readonly DbSet<T> _entities;
+        public EfRepository(AppDbContext context)
+        {
+            _context = context;
+            _entities = context.Set<T>();
+        }
+        public async Task AddAsync(T entity, CancellationToken cancellationToken = default)
+        {
+            await _entities.AddAsync(entity, cancellationToken);
+        }
+
+        public Task DeleteAsync(T entity, CancellationToken cancellationToken = default)
+        {
+            _entities.Remove(entity);
+            return Task.CompletedTask;
+        }
+
+        public async Task<T?> FirstOrDefaultAsync(Expression<Func<T, bool>> filter, CancellationToken cancellationToken = default)
+        {
+            return await _entities.FirstOrDefaultAsync(filter, cancellationToken);
+        }
+
+        public async Task<T?> GetByIdAsync(int id,
+            CancellationToken cancellationToken = default,
+            params Expression<Func<T, object>>[]? includesProperties)
+        {
+            IQueryable<T> query = _entities;
+            if (includesProperties != null)
+            {
+                foreach (var include in includesProperties)
+                {
+                    query = query.Include(include);
+                }
+            }
+            return await query.FirstOrDefaultAsync(e => e.Id == id, cancellationToken);
+        }
+
+        public async Task<IReadOnlyList<T>> ListAllAsync(CancellationToken cancellationToken = default)
+        {
+            IQueryable<T>? query = _entities.AsQueryable();
+            return await query.ToListAsync(cancellationToken);
+        }
+
+        public async Task<IReadOnlyList<T>> ListAsync(Expression<Func<T, bool>> filter, 
+            CancellationToken cancellationToken = default, 
+            params Expression<Func<T, object>>[]? includesProperties)
+        {
+            IQueryable<T>? query = _entities.AsQueryable();
+            if (includesProperties != null && includesProperties.Any())
+            {
+                foreach (Expression<Func<T, object>>? included in includesProperties)
+                {
+                    query = query.Include(included);
+                }
+            }
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            return await query.ToListAsync(cancellationToken);
+        }
+
+        public Task UpdateAsync(T entity, CancellationToken cancellationToken = default)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+            return Task.CompletedTask;
+        }
+    }
+}
