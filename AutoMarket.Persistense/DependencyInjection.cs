658a0839349de84e36b85492a9fa82f84e712d9a
John Lapunou <lapunoujohn@gmail.com>
2026-03-22 23:44:30 +0300

    Persistense: Added DI for registration in UI

---
+++ AutoMarket.Persistense/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using AutoMarket.Persistense.Data;
+using AutoMarket.Persistense.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AutoMarket.Persistense
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddFakePersistence(this IServiceCollection services)
+        {
+            services.AddSingleton<IUnitOfWork, FakeUnitOfWork>();
+            return services;
+        }
+
+        public static IServiceCollection AddPersistence(this IServiceCollection services, DbContextOptions<AppDbContext> options)
+        {
+            services.AddSingleton<IUnitOfWork, EfUnitOfWork>();
+            services.AddSingleton(new AppDbContext(options));
+            return services;
+        }
+    }
+}
