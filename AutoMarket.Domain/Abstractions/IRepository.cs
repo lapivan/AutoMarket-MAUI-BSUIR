400f13f4e023ed2803c673ecbbc18523c7e80a78
John Lapunou <lapunoujohn@gmail.com>
2026-03-22 18:57:01 +0300

    Domain: Added directory Abstractions, Created IRepository interface

---
+++ AutoMarket.Domain/Abstractions/IRepository.cs
@@ -0,0 +1,28 @@
+﻿using AutoMarket.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace AutoMarket.Domain.Abstractions
+{
+    public interface IRepository<T> where T:Entity 
+    {
+        Task<T?> GetByIdAsync(int id, 
+            CancellationToken cancellationToken = default, 
+            params Expression<Func<T, object>>[]? includesProperties);
+
+        Task<T?> FirstOrDefaultAsync(Expression<Func<T, bool>> filter,
+            CancellationToken cancellationToken = default);
+
+        Task<IReadOnlyList<T>> ListAllAsync(CancellationToken cancellationToken = default);
+
+        Task<IReadOnlyList<T>> ListAsync(Expression<Func<T, bool>> filter,
+            CancellationToken cancellationToken = default,
+            params Expression<Func<T, object>>[]? includesProperties);
+
+        Task AddAsync(T entity, CancellationToken cancellationToken = default);
+        Task UpdateAsync(T entity, CancellationToken cancellationToken = default);
+        Task DeleteAsync(T entity, CancellationToken cancellationToken = default);
+    }
+}
