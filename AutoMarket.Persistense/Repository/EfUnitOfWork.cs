5cf42b023530cd7cdc5c9996b9779ff884b830b2
John Lapunou <lapunoujohn@gmail.com>
2026-03-22 23:09:52 +0300

    Persistense: Added EfUnitOfWork

---
+++ AutoMarket.Persistense/Repository/EfUnitOfWork.cs
@@ -0,0 +1,25 @@
+﻿using AutoMarket.Persistense.Data;
+
+namespace AutoMarket.Persistense.Repository
+{
+    public class EfUnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+        private readonly Lazy<IRepository<CarBrand>> _brandRepository;
+        private readonly Lazy<IRepository<CarAnnouncement>> _announcementRepository;
+
+        public EfUnitOfWork(AppDbContext context)
+        {
+            _context = context;
+            _brandRepository = new Lazy<IRepository<CarBrand>>(() => new EfRepository<CarBrand>(context));
+            _announcementRepository = new Lazy<IRepository<CarAnnouncement>>(() => new EfRepository<CarAnnouncement>(context));
+        }
+        public IRepository<CarBrand> BrandRepository => _brandRepository.Value;
+
+        public IRepository<CarAnnouncement> AnnouncementRepository => _announcementRepository.Value;
+
+        public async Task CreateDataBaseAsync() => await _context.Database.EnsureCreatedAsync();
+        public async Task DeleteDataBaseAsync() => await _context.Database.EnsureDeletedAsync();
+        public async Task SaveAllAsync() => await _context.SaveChangesAsync();
+    }
+}
