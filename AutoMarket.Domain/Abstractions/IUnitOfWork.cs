1394994478fb98ac2b16ddf3a772c512b6a10a1f
John Lapunou <lapunoujohn@gmail.com>
2026-03-22 19:11:31 +0300

    Domain: Added interface IUnitOfWork

---
+++ AutoMarket.Domain/Abstractions/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using AutoMarket.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoMarket.Domain.Abstractions
+{
+    public interface IUnitOfWork
+    {
+        IRepository<CarBrand> BrandRepository { get; }
+        IRepository<CarAnnouncement> AnnouncementRepository { get; }
+        Task SaveAllAsync();
+        Task DeleteDataBaseAsync();
+        Task CreateDataBaseAsync();
+    }
+}
